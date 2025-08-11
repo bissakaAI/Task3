86aaadf60612811df275d2094e9f06ab1d244cfe
Hamid <bissakabissakadop31@gmail.com>
2025-08-11 15:24:06 +0100

    my assignment task 3

---
+++ DataStructure/Hamid_jimoh_task3.py
@@ -0,0 +1,41 @@
+#number1
+#code to take input 
+#
+
+change_case = input("pleade input what you want in uppercase: ")
+print(change_case.upper())
+
+
+#number 2
+word = "python"
+print(f"{word[0]} and {word[-1]}")
+
+#number3 
+
+input("what is your name: ")
+print("\"Hello,!\" where is the user's input")
+
+#number 4
+word = "i am next"
+count = word.find(word[-1]) + 1
+print(count)
+
+
+#number5
+
+word = "Hello World"
+print(word.replace("World","Python"))
+
+
+#TASK2
+word = "check me in python"
+print(word.rindex("python"))
+
+#number 7
+#a program to reverse a string without using slicing
+word = "check this reverse"
+reversed_word= reversed(word)
+reversed_word2= "".join(reversed_word)
+print(reversed_word2)
+
+#number8
