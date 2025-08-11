7846b67a2a918d0c2e49072183444f0554c332cd
Hamid <bissakabissakadop31@gmail.com>
2025-08-11 15:52:03 +0100

    rush hour commit

--- DataStructure/Hamid_jimoh_task3.py
+++ DataStructure/Hamid_jimoh_task3.py
@@ -1,41 +1,68 @@
-#number1
-#code to take input 
-#
+# #number1
+# #code to take input 
+# #
 
-change_case = input("pleade input what you want in uppercase: ")
-print(change_case.upper())
-
-
-#number 2
-word = "python"
-print(f"{word[0]} and {word[-1]}")
-
-#number3 
-
-input("what is your name: ")
-print("\"Hello,!\" where is the user's input")
-
-#number 4
-word = "i am next"
-count = word.find(word[-1]) + 1
-print(count)
+# change_case = input("pleade input what you want in uppercase: ")
+# print(change_case.upper())
 
 
-#number5
+# #number 2
+# word = "python"
+# print(f"{word[0]} and {word[-1]}")
 
-word = "Hello World"
-print(word.replace("World","Python"))
+# #number3 
+
+# input("what is your name: ")
+# print("\"Hello,!\" where is the user's input")
+
+# #number 4
+# word = "i am next"
+# count = word.find(word[-1]) + 1
+# print(count)
 
 
-#TASK2
-word = "check me in python"
-print(word.rindex("python"))
+# #number5
 
-#number 7
-#a program to reverse a string without using slicing
-word = "check this reverse"
-reversed_word= reversed(word)
-reversed_word2= "".join(reversed_word)
-print(reversed_word2)
+# word = "Hello World"
+# print(word.replace("World","Python"))
 
-#number8
+
+# #TASK2
+# word = "check me in python"
+# print(word.rindex("python"))
+
+# #number 7
+# #a program to reverse a string without using slicing
+# word = "check this reverse"
+# reversed_word= reversed(word)
+# reversed_word2= "".join(reversed_word)
+# print(reversed_word2)
+
+# #number8
+# word = "    i love allah   "
+# print(word.strip())
+
+# #number 9
+# user_input=input("input any sentense: ").lower()
+# vowels= ("a","e","i","o","u")
+# vowel_a = user_input.count("a")
+# vowel_e = user_input.count("e")
+# vowel_i = user_input.count("i")
+# vowel_o = user_input.count("o")
+# vowel_u = user_input.count("u")
+
+# vowel_count = vowel_a + vowel_e + vowel_i +vowel_o +vowel_u
+# print(vowel_count)
+
+
+#number 10
+string_word = "1234"
+string_conv = int(string_word)
+multi_string = string_conv * 2 
+print(multi_string)
+
+
+
+#TASK3
+
+input
